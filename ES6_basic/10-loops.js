02513986e12ef5d5b1e1cefd7e809e25cd1c9d84
Albinnich <albina_haji@hotmail.com>
2024-07-20 20:47:27 +0000

    For/of

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+export default function appendToEachArrayValue(array, appendString) {
+  for (let [idx, value] of array.entries()) {
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
