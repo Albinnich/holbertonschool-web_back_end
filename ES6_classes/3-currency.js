51587225dfcc9bf2bb481b07149289fd6d436a63
Albinnich <albina_haji@hotmail.com>
2024-07-21 13:51:28 +0000

    Fixed codes

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,32 +1,37 @@
 export default class Currency {
-	constructor(code, name) {
-		this.code = code;
-		this.name = name;
-	}
+  constructor(code, name) {
+    this._code = '';
+    this._name = '';
 
-	get code() {
-		return this._code;
-	}
+    this.code = code;
+    this.name = name;
+  }
 
-	set code(value) {
-		if (typeof value !== 'string') {
-			throw new TypeError('Code must be a string');
-		}
-		this._code = value;
-	}
+  get name() {
+    return this._name;
+  }
 
-	get name() {
-		return this._name;
-	}
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
 
-	set name(value) {
-		if (typeof value !== 'string') {
-			throw new TypeError('Name must be a string');
-		}
-		this._name = value;
-	}
+  get code() {
+    return this._code;
+  }
 
-	displayFullCurrency() {
-		return '${this.name} (${this.code})';
-	}
+  set code(value) {
+    if (typeof value === 'string') {
+      this._code = value;
+    } else {
+      throw new TypeError('Code must be string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
 }
