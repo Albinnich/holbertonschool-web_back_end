4ced510b03f07dec5d860335233c629186c8e418
Albinnich <albina_haji@hotmail.com>
2024-07-22 12:14:39 +0000

    0 and 1 tasks done

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,7 @@
-function getListStudents() {
-	const arrayOfObjects = [
+export default function getListStudents() {
+	return [
 		{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 		{ id: 2, firstName: 'James', location: 'Columbia' },
 		{ id: 3, firstName: 'Serena', location: 'San Francisco' }
 	];
-
-	return arrayOfObjects;
 }
