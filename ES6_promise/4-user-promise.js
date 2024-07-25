34ca35104510a62cd5b797cef6f769e8a4fb6ebb
Albinnich <albina_haji@hotmail.com>
2024-07-25 16:56:36 +0400

    code fixed

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,10 +1,4 @@
 /* eslint-disable */
-function signUpUser(firstName, lastName) {
-signUpUser('John', 'Doe')
-  .then(user => {
-    console.log('User signed up:', user);
-  })
-  .catch(error => {
-    console.error('Error signing up:', error);
-  });
-
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
+}
