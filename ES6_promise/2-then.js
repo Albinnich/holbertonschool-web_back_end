47e8ca2c2ab63e7e8cc7c4a9872ef257825e00e5
Albinnich <albina_haji@hotmail.com>
2024-07-25 14:13:19 +0400

    handle response

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+function handleResponseFromAPI(promise) {
+  promise.then(
+    (response) => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success'
+      };
+     },
+     (error) => {
+       console.log('Got a response from the API');
+       return new Error();
+      }
+  );
+}
+
+export default handleResponseFromAPI;
+
