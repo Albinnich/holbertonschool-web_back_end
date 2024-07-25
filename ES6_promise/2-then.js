93280936b65b32d84c62b4619ddea221d033a0ea
Albinnich <albina_haji@hotmail.com>
2024-07-25 14:25:24 +0400

    Clear

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,22 +1,4 @@
 /* eslint-disable */
-/*function handleResponseFromAPI(promise) {
-  promise.then(
-    (response) => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success'
-      };
-     },
-     (error) => {
-       console.log('Got a response from the API');
-       return new Error();
-      }
-  );
-}
-
-export default handleResponseFromAPI;*/
-
 function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
