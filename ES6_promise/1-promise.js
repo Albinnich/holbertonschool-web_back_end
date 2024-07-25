8195129912c9b1df9a65654015d379f20cb9fb6b
Albinnich <albina_haji@hotmail.com>
2024-07-25 13:39:41 +0400

    boolean

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+function getFullResponseFromApi(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+	body: 'Success'
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+//export default getFullResponseFromAPI;
