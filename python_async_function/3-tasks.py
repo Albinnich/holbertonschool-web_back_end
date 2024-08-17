7956a98b9d1058ebbbe36dd12795881e986497f8
Albinnich <albina_haji@hotmail.com>
2024-08-17 20:27:14 +0000

    No asyncio

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+Module on regular function syntax
+'''
+
+
+import asyncio
+from 0-basic_async_syntax import wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''
+    Returns an asyncio
+    '''
+    return asyncio.create_task(wait_random(max_delay))
