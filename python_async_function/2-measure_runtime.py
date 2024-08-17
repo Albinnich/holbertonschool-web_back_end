1526edaead9f1041126dfb34b53291e753f71d4f
Albinnich <albina_haji@hotmail.com>
2024-08-17 20:42:29 +0000

    modules fixed

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -5,7 +5,7 @@ Module to measure runtime
 import time
 import asyncio
 from typing import List
-from 1-concurrent_coroutines import wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
