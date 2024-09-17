0dd8b2bdd647685f0b7c14d7f3fa36c5e7b39a6f
Albinnich <albina_haji@hotmail.com>
2024-09-17 10:41:27 +0000

    i hate lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,13 +3,12 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n').filter((line) => line.trim() !== ''); // No trailing spaces here
-    
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     if (lines.length === 0) {
       throw new Error('Cannot load the database');
     }
 
-    const students = lines.slice(1); // Skip the header
+    const students = lines.slice(1);
     console.log(`Number of students: ${students.length}`);
 
     const fields = {};
