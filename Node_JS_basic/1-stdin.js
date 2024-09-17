b43bee17ad2bd7ec76924a8f4f6acd8544cf3b71
Albinnich <albina_haji@hotmail.com>
2024-09-17 10:15:49 +0000

    task 1 is fixed

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,8 @@
-// Display welcome message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-// Read input from the user
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim(); // Trim to remove trailing newline characters
-  console.log(`Your name is: ${name}`);
-
-  // End the input stream and display the closing message
-  process.stdin.end(); // Properly end the input stream
-});
-
-// Listen for the 'end' event to display the closing message
-process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write("This important software is now closing\n");
+  process.exit();
 });
